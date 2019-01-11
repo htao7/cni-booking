29852dc28d5141e0fd2b8d50f1f46fd1eafcf397
taotao720 <44954302+taotao720@users.noreply.github.com>
2019-01-11 18:04:32 -0500

    Delete run.bat

--- Booking/run.bat
+++
@@ -1,6 +0,0 @@
-@echo off                           //Turn off screen text messages
-:loop                               //Set marker called loop, to return to
-python booking.py
-timeout /t 600 >null               //Wait 20 minutes
-(optional)
-goto loop
